469dc0d8871bceaf4fd39410ec1e4df5848bbdea
Siddhant-Thaware <siddhantthaware2025.etc@mmcoe.edu.in>
2026-09-07 11:34:52 +0530

    Update 1.  Pascal Triangle.cpp

--- Practical 1/1.  Pascal Triangle.cpp
+++ Practical 1/1.  Pascal Triangle.cpp
@@ -1,34 +1,57 @@
+/*
+Name: Siddhant Thaware
+PRN: B25ET1056
+Batch: C1
+*/
+
 #include <iostream>
 using namespace std;
-int fact (int p)
+
+// Function to calculate factorial
+int fact(int p)
 {
-    int res=1;
-    for(int i=p;i>1;i--)
+    int res = 1;
+
+    for(int i = p; i > 1; i--)
     {
-        res*=i;
+        res *= i;
     }
+
     return res;
 }
- int Binocoef(int n,int r)
- {
-     int coef;
-     coef = fact(n) / (fact(r) * fact(n-r));
-     return coef;
- }
- void printTriangle(int n)
- {
-     for(int l=0;l<n;l++)
-     {for(int m=0;m<=l;m++)
-     {cout<<"\t"<<Binocoef(l,m);}
-     cout<<"\n";}
- }
- int main()
- {
-     int n;
-     cout<<"enter the no. of rows:";
-     cin>>n;
-     
-     printTriangle(n);
-     
-     return 0;
- }
+
+// Function to calculate binomial coefficient
+int Binocoef(int n, int r)
+{
+    int coef;
+
+    coef = fact(n) / (fact(r) * fact(n-r));
+
+    return coef;
+}
+
+// Function to print Pascal Triangle
+void printTriangle(int n)
+{
+    for(int l = 0; l < n; l++)
+    {
+        for(int m = 0; m <= l; m++)
+        {
+            cout << "\t" << Binocoef(l, m);
+        }
+
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int n;
+
+    cout << "enter the no. of rows:";
+    cin >> n;
+
+    printTriangle(n);
+
+    return 0;
+}
